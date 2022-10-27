78c45087482df4e5b373c745d436b6f8dc4e6c07
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-10-27 21:07:42 +0000

    freakin alignment

--- src/character.py
+++ src/character.py
@@ -12,22 +12,26 @@ class Char:
         "intelligence": 10,
         "charisma": 10
     }
+    ALIGNMENT = {'lawful good', 'lawful neutral', 'lawful evil', 'neutral good', 'true neutral', 'neutral evi', 'chaotic good', 'chaotic neutral', 'chaotic evil'}
     def __init__(self, name, **kwargs):
         self.name = name
-        self.align= 'neutral'
         self.armor = 10
         self.hit_points = 5
         self.xp = 0
         self.level = 1
         self.attack_value = 1
-
         for a in Char.ABILITIES:
             if a in kwargs:
                 value = kwargs[a]
             else:
                 value = Char.ABILITIES[a]
-            
             setattr(self, a, Abilities(value))
+        for a in Char.ALIGNMENT:
+            if a in kwargs:
+                self.align = a
+            else:
+                self.align = 'true neutral'
+            
 
 
 
